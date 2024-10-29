6f045cea7277b7010d69544a7aaf77fc65c44e51
Erik Syniawa <e.syniawa@gmx.de>
2024-10-29 10:35:18 +0100

    Added norm in plot

--- run_gain.py
+++ run_gain.py
@@ -53,22 +53,41 @@ if __name__ == '__main__':
                pop_monitor=pops_monitor_test,
                save_path='results/' + folder)
 
-    fig, axs = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+    fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(8, 7))
     plt.subplots_adjust(wspace=0)  # Set horizontal spacing to 0
     for scale, alpha in zip(gain_signals, alpha_values):
         dict = pops_monitor_test.load(folder='results/' + folder + f'gain_scale{scale}/')
-        axs[0].plot(dict['r_M1'][-1, :, 0], label=f'D1 scale = {round(scale, 2)}', color='orange', linewidth=2, alpha=alpha)
-        axs[1].plot(dict['r_M1'][-1, :, 1], label=f'D1 scale = {round(scale, 2)}', color='blue', linewidth=2, alpha=alpha)
+        axs[0, 0].plot(dict['r_M1'][-1, :, 0], label=f'D1 scale = {round(scale, 2)}', color='orange', linewidth=2, alpha=alpha)
+        axs[0, 1].plot(dict['r_M1'][-1, :, 1], label=f'D1 scale = {round(scale, 2)}', color='blue', linewidth=2, alpha=alpha)
 
-    axs[0].set_xlabel('Neuron $\\theta_{shoulder}$ in [°]', fontsize=12), axs[1].set_xlabel('Neuron $\\theta_{elbow}$', fontsize=12)
-    axs[0].set_ylabel('r', fontsize=12)
+    axs[0, 0].set_xlabel('Encoding neuron $\\theta_{shoulder}$ in [°]', fontsize=12), axs[0, 1].set_xlabel('Encoding neuron $\\theta_{elbow} in [°]$', fontsize=12)
+    axs[0, 0].set_ylabel('r', fontsize=12)
 
     xlabels = [ang if i % 2 == 0 else None for i, ang in enumerate(parameters['motor_orientations'])]
-    axs[0].set_xticks(np.arange(0, parameters['dim_motor'][0]), xlabels)
-    axs[1].set_xticks(np.arange(0, parameters['dim_motor'][0]), xlabels),
-    axs[0].xaxis.set_label_position('top'), axs[1].xaxis.set_label_position('top')
+    ylabels = np.round(np.linspace(0, 0.5, 6, endpoint=True), 1)
+    axs[0, 0].set_xticks(np.arange(0, parameters['dim_motor'][0]), xlabels)
+    axs[0, 1].set_xticks(np.arange(0, parameters['dim_motor'][0]), xlabels),
+    axs[0, 0].xaxis.set_label_position('top'), axs[0, 1].xaxis.set_label_position('top')
+    axs[0, 0].set_ylim(0, 0.5), axs[0, 1].set_ylim(0, 0.5)
+    axs[0, 0].legend(), axs[0, 1].legend()
+    axs[0, 0].set_yticks(ylabels, ylabels), axs[0, 1].set_yticks(ylabels, [None for _ in ylabels])
 
-    axs[0].legend(), axs[1].legend()
+    # norm plots
+    ylabels = np.round(np.linspace(0, 1.5, 6, endpoint=True), 1)
+    norm_shoulder = []
+    norm_elbow = []
+    for scale, alpha in zip(gain_signals, alpha_values):
+        dict = pops_monitor_test.load(folder='results/' + folder + f'gain_scale{scale}/')
+        norm_shoulder.append(np.sqrt(dict['r_Output_Shoulder'][-1, 1]**2 + dict['r_Output_Shoulder'][-1, 2]**2))
+        norm_elbow.append(np.sqrt(dict['r_Output_Elbow'][-1, 1]**2 + dict['r_Output_Elbow'][-1, 2]**2))
+
+    axs[1, 0].plot(gain_signals, norm_shoulder, color='orange', linewidth=2)
+    axs[1, 1].plot(gain_signals, norm_elbow, color='blue', linewidth=2)
+    axs[1, 0].set_xlabel('Gain'), axs[1, 1].set_xlabel('Gain')
+    axs[1, 0].set_ylabel('$r_{norm}$')
+    axs[1, 0].set_xticks(gain_signals, np.round(gain_signals, 1)), axs[1, 1].set_xticks(gain_signals, np.round(gain_signals, 1))
+    axs[1, 0].set_ylim(0, 1.5), axs[1, 1].set_ylim(0, 1.5)
+    axs[1, 0].set_yticks(ylabels, ylabels), axs[1, 1].set_yticks(ylabels, [None for _ in ylabels])
 
     plt.savefig('results/' + folder + 'd1_gain.pdf', bbox_inches='tight', pad_inches=0, dpi=300)
     if exp_args.display:
